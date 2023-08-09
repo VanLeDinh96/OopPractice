74f692e0bee423ceac3d9fc34f192019ac40d291
diegovanne <dinhvanle.it@gmail.com>
2023-08-09 17:35:59 +0700

    update

---
+++ Practice/Polymorphim2/Calculator.cs
@@ -0,0 +1,18 @@
+namespace Practice.Polymorphim2
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public string Add(string a, string b)
+        {
+            return a + b;
+        }
+    }
+}
--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,12 +1,23 @@
-﻿using Practice.Polymorphism;
+﻿
+// polymorphism 1:
+// Shape[] shapes = new Shape[]
+// {
+//     new Circle(),
+//     new Square()
+// };
 
-// polymorphism
-Shape[] shapes = new Shape[]
-{
-    new Circle(),
-    new Square()
-};
+// foreach(var shape in shapes){
+//     shape.Draw();
+// }
 
-foreach(var shape in shapes){
-    shape.Draw();
-}
+// polymorphism 2:
+using Practice.Polymorphim2;
+
+Calculator calculator = new Calculator();
+int result1 = calculator.Add(1, 2);
+double result2 = calculator.Add(2.2,3.3);
+string result3 = calculator.Add("hello ", "world!");
+
+Console.WriteLine(result1);
+Console.WriteLine(result2);
+Console.WriteLine(result3);
